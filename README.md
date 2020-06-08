6f919223374a4f2ed2664667635ad02399b3113b
MauricioSelfene <m.selfene@gmail.com>
2020-06-07 21:40:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-cbrauv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cbrauv)
---
+++ index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+
+function MiComponente(){
+  return <p> Hola mundo </p>;
+}
+
+
+class MiComponenteDeClase extends Component{
+  render(){
+    return <p> Hola soy de la clase </p>;
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+         < MiComponente />
+      </div>
+    );
+  }
+}
+//podemoos retornar con render la funcion o la clase 
+render(<App />, document.getElementById('root'));
